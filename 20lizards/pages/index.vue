92c808da6260b22b5253ab9c2f871ea3dfa859fc
Shunya078 <Shunya078@users.noreply.github.com>
2020-11-21 09:07:20 +0000

    Format

--- 20lizards/pages/index.vue
+++ 20lizards/pages/index.vue
@@ -2,35 +2,36 @@
   <div class="hello">
     <template>
       <v-carousel cycle height="300" show-arrows-on-hover>
-        <v-carousel-item v-for="(item,i) in images" :key="i" :src="item.src" reverse-transition="fade-transition"
-      transition="fade-transition">
+        <v-carousel-item
+          v-for="(item, i) in images"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        >
         </v-carousel-item>
       </v-carousel>
     </template>
     <div style="margin-top: 20px"></div>
-    <h2 class="text-center" style="font-family: 'ヒラギノ丸ゴ Pro'">{{ msg }}</h2>
+    <h2 class="text-center" style="font-family: 'ヒラギノ丸ゴ Pro'">
+      {{ msg }}
+    </h2>
     <v-simple-table>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">
-          </th>
-          <th class="text-left">
-            Title
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in blogs"
-          :key="item.name"
-        >
-          <td>{{ item.name }}</td>
-          <td>{{ item.name }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left"></th>
+            <th class="text-left">Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in blogs" :key="item.name">
+            <td>{{ item.name }}</td>
+            <td>{{ item.name }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </div>
 </template>
 
@@ -45,64 +46,69 @@ export default {
     return {
       msg: "名古屋大学男子ラクロス部 20Lizards",
       images: [
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872103-c88b1380-2c22-11eb-810d-ff4ddcfa0d70.jpg',
-          },
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872106-cd4fc780-2c22-11eb-8da1-a4b611ac505e.jpg',
-          },
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872107-d0e34e80-2c22-11eb-9299-ee4e9abf08e8.jpg',
-          },
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872108-d2147b80-2c22-11eb-85f8-b9d296fbeb53.jpg',
-          },
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872109-d2ad1200-2c22-11eb-888e-32c290bc0fc3.jpg',
-          },
-        ],
-        blogs: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-          },
-        ],
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872103-c88b1380-2c22-11eb-810d-ff4ddcfa0d70.jpg",
+        },
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872106-cd4fc780-2c22-11eb-8da1-a4b611ac505e.jpg",
+        },
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872107-d0e34e80-2c22-11eb-9299-ee4e9abf08e8.jpg",
+        },
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872108-d2147b80-2c22-11eb-85f8-b9d296fbeb53.jpg",
+        },
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872109-d2ad1200-2c22-11eb-888e-32c290bc0fc3.jpg",
+        },
+      ],
+      blogs: [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+        },
+        {
+          name: "Donut",
+          calories: 452,
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+        },
+      ],
     };
   },
   methods: {
@@ -129,7 +135,7 @@ export default {
 h1,
 h2 {
   font-weight: normal;
-  font-family: 'ヒラギノ丸ゴ Pro';
+  font-family: "ヒラギノ丸ゴ Pro";
 }
 ul {
   list-style-type: none;
