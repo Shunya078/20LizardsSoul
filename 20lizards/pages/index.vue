9ca9998b7ea39728051138c3eaeb9208af08a4f1
Shunya078 <Shunya078@users.noreply.github.com>
2020-11-21 08:59:00 +0000

    Format

--- 20lizards/pages/index.vue
+++ 20lizards/pages/index.vue
@@ -2,8 +2,13 @@
   <div class="hello">
     <template>
       <v-carousel cycle height="300" show-arrows-on-hover>
-        <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" reverse-transition="fade-transition"
-      transition="fade-transition">
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        >
         </v-carousel-item>
       </v-carousel>
     </template>
@@ -28,22 +33,27 @@ export default {
     return {
       msg: "Welcome to Your Vue.js App",
       items: [
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872103-c88b1380-2c22-11eb-810d-ff4ddcfa0d70.jpg',
-          },
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872106-cd4fc780-2c22-11eb-8da1-a4b611ac505e.jpg',
-          },
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872107-d0e34e80-2c22-11eb-9299-ee4e9abf08e8.jpg',
-          },
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872108-d2147b80-2c22-11eb-85f8-b9d296fbeb53.jpg',
-          },
-          {
-            src: 'https://user-images.githubusercontent.com/50067752/99872109-d2ad1200-2c22-11eb-888e-32c290bc0fc3.jpg',
-          },
-        ],
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872103-c88b1380-2c22-11eb-810d-ff4ddcfa0d70.jpg",
+        },
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872106-cd4fc780-2c22-11eb-8da1-a4b611ac505e.jpg",
+        },
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872107-d0e34e80-2c22-11eb-9299-ee4e9abf08e8.jpg",
+        },
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872108-d2147b80-2c22-11eb-85f8-b9d296fbeb53.jpg",
+        },
+        {
+          src:
+            "https://user-images.githubusercontent.com/50067752/99872109-d2ad1200-2c22-11eb-888e-32c290bc0fc3.jpg",
+        },
+      ],
     };
   },
   methods: {
