b10b16ce8f5fdd2b8e06b4ef12a7375d94c1c260
Shunya078 <Shunya078@users.noreply.github.com>
2020-11-21 10:12:46 +0000

    Format

--- 20lizards/pages/post.vue
+++ 20lizards/pages/post.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="post">
     <p v-if="showMessage" style="margin-top: 20px">
-          フォームが正常に送信されました。
+      フォームが正常に送信されました。
     </p>
     <v-card class="mx-auto my-12" width="374">
       <v-form ref="form">
         <v-card-title>ブログ投稿</v-card-title>
-          <v-col cols="12" md="6">
-            <h3>タイトル</h3>
-            <v-text-field
-              type="text"
-              placeholder="Title"
-              v-model="title"
-              required
-            />
-            <h3>本文記入欄</h3>
-            <v-textarea
-              type="message"
-              placeholder="本文を記入"
-              v-model="message"
-              required
-            />
-          </v-col>
+        <v-col cols="12" md="6">
+          <h3>タイトル</h3>
+          <v-text-field
+            type="text"
+            placeholder="Title"
+            v-model="title"
+            required
+          />
+          <h3>本文記入欄</h3>
+          <v-textarea
+            type="message"
+            placeholder="本文を記入"
+            v-model="message"
+            required
+          />
+        </v-col>
         <v-card-actions class="d-flex justify-center mb-8">
           <v-btn color="warning" @click="postBlog" large width="200">
             投稿する
@@ -48,20 +48,23 @@ export default {
     postBlog: function () {
       const colref = firestore.collection("blog");
 
-     // 保存用JSONデータを作成
+      // 保存用JSONデータを作成
       const saveData = {
         title: this.title,
-        message: this.message
+        message: this.message,
       };
 
-     // addの引数に保存したいデータを渡す
-      colref.add(saveData).then(function(docRef) {
+      // addの引数に保存したいデータを渡す
+      colref
+        .add(saveData)
+        .then(function (docRef) {
           // 正常にデータ保存できた時の処理
           console.log("Document written with ID: ", docRef.id);
-      }).catch(function(error) {
+        })
+        .catch(function (error) {
           // エラー発生時の処理
           console.error("Error adding document: ", error);
-      });
+        });
 
       this.showMessage = true;
     },
