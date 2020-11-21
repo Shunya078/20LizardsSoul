cb800c140d44418173d51e89edca0855400254ce
Shunya078 <kikukan007@gmail.com>
2020-11-21 17:58:13 +0900

    add carousel

--- 20lizards/pages/index.vue
+++ 20lizards/pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="hello">
+    <template>
+      <v-carousel cycle height="300" show-arrows-on-hover>
+        <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" reverse-transition="fade-transition"
+      transition="fade-transition">
+        </v-carousel-item>
+      </v-carousel>
+    </template>
+    <div style="margin-top: 20px"></div>
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <button @click="signOut">Sign out</button>
@@ -19,6 +27,23 @@ export default {
   data() {
     return {
       msg: "Welcome to Your Vue.js App",
+      items: [
+          {
+            src: 'https://user-images.githubusercontent.com/50067752/99872103-c88b1380-2c22-11eb-810d-ff4ddcfa0d70.jpg',
+          },
+          {
+            src: 'https://user-images.githubusercontent.com/50067752/99872106-cd4fc780-2c22-11eb-8da1-a4b611ac505e.jpg',
+          },
+          {
+            src: 'https://user-images.githubusercontent.com/50067752/99872107-d0e34e80-2c22-11eb-9299-ee4e9abf08e8.jpg',
+          },
+          {
+            src: 'https://user-images.githubusercontent.com/50067752/99872108-d2147b80-2c22-11eb-85f8-b9d296fbeb53.jpg',
+          },
+          {
+            src: 'https://user-images.githubusercontent.com/50067752/99872109-d2ad1200-2c22-11eb-888e-32c290bc0fc3.jpg',
+          },
+        ],
     };
   },
   methods: {
